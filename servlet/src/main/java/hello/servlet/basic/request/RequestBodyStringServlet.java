9668242632b4ba425b565aeb9f04e02a5e61c11b
ajfldkvmek <slowloris1004@gmail.com>
2024-07-04 15:50:51 +0900

    spring study

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8); //이 경우 plaintext로 반환이 됨 요즘은 JSON이다 이 새끼들아
+
+        System.out.println("messageBody = " +messageBody);
+
+        response.getWriter().write("ok");
+    }
+}
