9ee4d9255bbab548abe764489148a0266331c0c2
ajfldkvmek <slowloris1004@gmail.com>
2025-01-09 22:56:39 +0900

    25.01.09

---
+++ form-start/src/main/java/hello/itemservice/FormStartApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormStartApplication.class, args);
+	}
+
+}
